61fa8477a86effbefe7eb1fd3d31d0c4c956f71c
JoshOY <JoshOY789@gmail.com>
2015-04-02 20:50:09 +0800

    Update some components on ShoppingPage.

--- src/components/ShoppingPage/ShoppingPage.js
+++ src/components/ShoppingPage/ShoppingPage.js
@@ -3,10 +3,34 @@
 import React from 'react';
 import Banner from '../Banner';
 
+var cartItemNum = 0;
+
+var CartIcon = React.createClass({
+  render: function() {
+    return (
+      <div className="cart-page-cart-container">
+        <a><img src={require('./iconfont-gouwuche.png')} className="cart-page-cart-icon"></img></a>
+        <span className="cart-page-cart-num">{cartItemNum}</span>
+      </div>
+    );
+  }
+});
+
+var GreenNav = React.createClass({
+  render: function(){
+    return (
+      <div className="green-nav">
+        <CartIcon />
+      </div>
+    );
+  }
+});
+
 const ShoppingPage = React.createClass({
   render() {
     return (
-      <div>
+      <div className="cart-page-main-container">
+        <GreenNav />
         <p>this is shopping!!!!!!!!</p>
       </div>
     );
