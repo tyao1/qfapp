335960659634bea691da3ab6b4e52bafccc89e50
JoshOY <JoshOY789@gmail.com>
2015-04-08 01:37:34 +0800

    Update shopping cart component.

--- src/components/ShoppingPage/ShoppingPage.js
+++ src/components/ShoppingPage/ShoppingPage.js
@@ -12,7 +12,7 @@ var CartIcon = React.createClass({
     return (
       <div className="cart-page-cart-container">
         <a><img src={require('./iconfont-gouwuche.png')} className="cart-page-cart-icon"></img></a>
-        <span className="cart-page-cart-num">{cartItemNum}</span>
+        <span className="cart-page-cart-num">{cartItemNum.toString()}</span>
       </div>
     );
   }
@@ -22,7 +22,78 @@ var GreenNav = React.createClass({
   render: function(){
     return (
       <div className="green-nav">
+        <span className="gouwuche-title">购物车</span>
         <CartIcon />
+      </div>
+    );
+  }
+});
+
+var Counter = React.createClass({
+  render: function(){
+    // CSS 该怎么找啊啊~~~
+    return (
+      <div>
+        <form>
+          <input type="button" value="-"></input>
+          <input type="number" value={this.props.initValue}></input>
+          <input type="button" value="+"></input>
+        </form>
+      </div>
+    );
+  }
+});
+
+var CartListItem = React.createClass({
+  render: function(){
+    // 价格部分的逻辑还要改啊啊啊！
+    return (
+      <li>
+        <div className={"cart-list-item-container " + (this.props.lastChild ? "lastItem" : "")}>
+          <img src={require(this.props.image)} />
+          <span className="cart-list-item-simple-info">
+            <h3>{this.props.itemtype}</h3>
+            <p>{this.props.itemname}</p>
+          </span>
+
+          <span className="cart-list-item-simple-info">
+            <h3>数量</h3>
+            <Counter initValue={1} />
+          </span>
+
+          <span className="cart-list-item-simple-info">
+            <h3>卖家</h3>
+            <p>{this.props.sellername}</p>
+          </span>
+
+          <span className="cart-list-item-simple-info">
+            <h3>价格</h3>
+            <p className="item-price-text">
+            {"￥ " + (this.props.price | 0).toString() + '.00'}
+            </p>
+          </span>
+
+          <span className="cart-list-item-others">
+            <div className="delete-item">
+              <a>删除</a>
+            </div>
+          </span>
+        </div>
+      </li>
+    );
+  }
+});
+
+var CartListComponent = React.createClass({
+  render: function(){
+    return (
+      <div className="cart-list-main-container">
+        <ul>
+          <CartListItem image="./test-book.png" itemname="论演员的自我修养啊自我修养啊自我修养啊"
+            itemtype="书籍" sellername="没名字能用了啊啊啊啊" price={24} lastChild={false}/>
+          <CartListItem image="./test-book.png" itemname="论演员的自我修养自我修养"
+            itemtype="书籍" sellername="没名字能用了啊啊啊啊" price={36} lastChild={true}/>
+        </ul>
       </div>
     );
   }
@@ -33,6 +104,7 @@ const ShoppingPage = React.createClass({
     return (
       <div className="cart-page-main-container">
         <GreenNav />
+        <CartListComponent />
         <p>this is shopping!!!!!!!!</p>
       </div>
     );
