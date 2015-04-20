91afeda1fe62b8daffd1916c227834e54d88a369
xiaobuu <yaotianyu0512@gmail.com>
2015-04-21 00:35:37 +0800

    Finish off submit page UI

--- src/components/ItemRegisterForm/ItemRegisterForm.js
+++ src/components/ItemRegisterForm/ItemRegisterForm.js
@@ -3,65 +3,128 @@
 import React from 'react';
 import Banner from '../Banner';
 
+import {close} from '../SVGs';
 
 require("./ItemRegisterForm.scss");
 
 
 const ItemRegisterForm = React.createClass({
+
+  getInitialState(){
+
+    return {
+      name: '',
+      price: '0.00',
+      num: 1,
+      timeSpan: 5,
+      detail: ''
+
+
+    };
+  },
+
+  handleNameChange(e){
+    this.setState({name:e.target.value})
+  },
+
+  handlePriceChange(e){
+    //price因为小数点原因，需要保留字符串状态
+    let price = e.target.value||0;
+    if(price==='.'){
+      //0.
+      this.setState({price: '0.'});
+    }
+    else {
+      const priceOnly = /^\d+\.?\d{0,2}$/;
+      if (priceOnly.test(price)) {
+        if (/^0\d+$/.test(price)) {
+          //移除无用0
+          price = price.substring(1);
+        }
+        //if (!isNaN(price)) {
+        this.setState({price: price})
+        //}
+      }
+    }
+  },
+
+  handleAmountChange(e){
+    let num = e.target.value;
+    if(!num){
+      this.setState({num:0});
+      }
+    else {
+      const numOnly = /^\d+$/;
+      if (numOnly.test(num)) {
+        //因为是数字，所以parseInt肯定工作，保留int状态
+        this.setState({num: parseInt(e.target.value)})
+      }
+    }
+  },
+  handleTimeSpanChange(e){
+    this.setState({timeSpan: e.target.value});
+  },
+  handleDetailChange(e){
+    this.setState({detail:e.target.value})
+  },
+  addAmount(){
+
+    this.setState({num: this.state.num+1})
+
+  },
+  reduceAmount(){
+    if(this.state.num>1){
+      this.setState({num: this.state.num-1})
+    }
+  },
+
   render: function() {
     return (
       <div className="itemRegisterForm">
-        <div className="border-clip-left"></div>
-
         <div className="left">
-          <div className="name-input-container">
-            <div className="group">
-              <input id="name-input" type="text" required />
-              <span className="highlight"></span>
-              <span className="bar"></span>
-              <label>物品名称</label>
+          <div className="inputEffectLong">
+            <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
+            <label className={this.state.name.length?'active':null} >物品名称</label>
+          </div>
+
+          <div className="inputEffect price">
+            <span>¥</span>
+            <input type="text" value={this.state.price} onChange={this.handlePriceChange}/>
+            <label className='active'>价格</label>
+          </div>
+
+
+          <div className="inputEffect">
+            <input type="text" value={this.state.num} onChange={this.handleAmountChange}/>
+            <label className='active'>数量</label>
+            <div className="controls">
+              <button onClick={this.addAmount}>+</button>
+              <button onClick={this.reduceAmount}>-</button>
             </div>
           </div>
 
-          <div className="other-inputs-container">
-            <div className="group">
-              <input type="text" required />
-              <span className="highlight"></span>
-              <span className="bar"></span>
-              <label>价格</label>
-            </div>
-
-            <div className="group">
-              <input type="text" required />
-              <span className="highlight"></span>
-              <span className="bar"></span>
-              <label>分类</label>
-            </div>
+          <div className="inputEffect">
+            <select value={this.state.timeSpan} onChange={this.handleTimeSpanChange}>
+              <option value="5">5个月</option>
+              <option value="4">4个月</option>
+              <option value="3">3个月</option>
+              <option value="2">2个月</option>
+              <option value="1">1个月</option>
+            </select>
+            <label className='active'>出售时长</label>
           </div>
 
-          <div className="other-inputs-container">
-            <div className="group">
-              <input type="text" required />
-              <span className="highlight"></span>
-              <span className="bar"></span>
-              <label>数量</label>
-            </div>
-
-            <div className="group">
-              <input type="text" required />
-              <span className="highlight"></span>
-              <span className="bar"></span>
-              <label>出售时长</label>
-            </div>
-          </div>
         </div>
 
-        <div className="delete">
-          <button><img src={require('./iconfont-close.svg')} /></button>
+        <div className="delete" onClick={this.props.onClose}>
+          {close}
         </div>
 
         <div className="right">
-          <textarea placeholder="详细描述"/>
+          <div className="textareaEffect">
+            <textarea value={this.state.detail} onChange={this.handleDetailChange}/>
+            <label className={this.state.detail.length?'active':null} >详细描述</label>
+          </div>
         </div>
 
       </div>
--- src/components/SellPage/SellPage.js
+++ src/components/SellPage/SellPage.js
@@ -5,8 +5,11 @@ import UserStore from '../../stores/UserStore'
 import PureRenderMixin from 'react/lib/ReactComponentWithPureRenderMixin';
 import UserConstants from '../../constants/UserConstants';
 
-import {boxface,additem} from '../SVGs';
+import {boxface,additem,paperplane} from '../SVGs';
 import ButtonNormal from '../ButtonNormal';
+import Modal from '../Modal';
+
+
 import ItemRegisterForm from '../ItemRegisterForm';
 import RequireLogin from '../../mixins/RequireLogin'
 require('./SellPage.scss');
@@ -19,7 +22,12 @@ const SellPage = React.createClass({
   mixins:[PureRenderMixin, RequireLogin],
   getInitialState(){
     return {
-      items:[]
+      items: ['d1'],
+      itemsCount: 1,
+      errMsg: '',
+      b_NO: '',
+      NO:'',
+      modalSubmitIsOpen: false
     }
   },
   componentWillMount(){
@@ -30,11 +38,84 @@ const SellPage = React.createClass({
     UserStore.removeChangeListener(this._onUserChange);
   },
 
-  itemsCount:0,
   handleAddClick(){
-    this.setState({items:this.state.items.concat('d'+this.itemsCount++)});
+    let count = this.state.itemsCount+1;
+    this.setState({
+      itemsCount: count,
+      items: this.state.items.concat('d'+count)
+    });
   },
+  goodsInfo:null,
   handleSubmitClick(){
+    let errMsg = '';
+
+    const items = this.state.items;
+    console.log(items);
+    this.goodsInfo = [];
+    let goodToGo = true;
+    items.map((data,i) => {
+      const state = this.refs[data].state;
+      if(!state.name||state.name.length<4){
+        errMsg+= `${i+1}号物品，物品名至少需要4个字<br/>`;
+        goodToGo = false;
+      }
+      else if(state.num<1){
+        console.log(state);
+        errMsg+= `${i+1}号物品，物品数量需要至少为1<br/>`;
+        goodToGo = false;
+      }
+      else{
+        if(goodToGo) {
+          //提交
+          console.log(state);
+          let t_limit = Date.now() / 1000 + parseInt(state.timeSpan) * 60 * 60 * 24 * 30;
+          let {name,price,detail,num} = state;
+          let good = {name, price, t_limit, detail, num};
+          this.goodsInfo.push(good);
+        }
+      }
+
+    });
+
+    if(goodToGo){
+      this.setState({
+        errMsg: '',
+        modalSubmitIsOpen: true
+      });
+      //call submit action
+
+    }
+    else{
+      this.setState({errMsg});
+    }
+
+  },
+  handleFormClose(data){
+    return ()=> {
+      let items = this.state.items;
+      let index = items.indexOf(data);
+      if (index > -1) {
+        items.splice(index, 1);
+        this.setState({
+          items,
+          itemsCount: this.state.itemsCount+1
+        });
+      }
+    }
+  },
+  handleModalSubmitClose(){
+    this.setState({modalSubmitIsOpen:false});
+  },
+  handleBNOChange(e){
+    this.setState({b_NO:e.target.value});
+
+  },
+  handleNOChange(e){
+    this.setState({NO:e.target.value});
+  },
+  handleRealSubmitClick(){
+
+
 
   },
   render(){
@@ -52,21 +133,42 @@ const SellPage = React.createClass({
           </div>
           <ul className="items">
             {
-              items.map((data) => <ItemRegisterForm key={data}/>)
+              items.map((data) => <ItemRegisterForm key={data} onClose={this.handleFormClose(data)} ref={data}/>)
             }
           </ul>
+
+          <p className={`err ${this.state.errMsg?'active':''}`} dangerouslySetInnerHTML={{__html:this.state.errMsg}}></p>
+
           <div className="controls">
 
             <ButtonNormal text="添加物品" svg={additem} onClick={this.handleAddClick}/>
 
-            <p></p>
             {
               items.length?
-              <ButtonNormal text="提交列表" svg={additem} onClick={this.handleSubmitClick}/>
+              <ButtonNormal className="ButtonNormal submit" text="提交详单" svg={paperplane} onClick={this.handleSubmitClick}/>
                 :null
             }
           </div>
         </div>
+
+
+        <Modal isOpen = {this.state.modalSubmitIsOpen} onClose = {this.handleModalSubmitClose}>
+          <div className="submitForm">
+            <p className="main">马上就好，还差一些信息</p>
+
+            <div className="inputEffectAgain">
+              <input type="text" value={this.state.b_NO} onChange={this.handleBNOChange}/>
+              <label className={this.state.b_NO.length?'active':null}>宿舍楼号</label>
+            </div>
+
+            <div className="inputEffectAgain">
+              <input type="text" value={this.state.NO} onChange={this.handleNOChange}/>
+              <label className={this.state.NO.length?'active':null}>宿舍号</label>
+            </div>
+
+            <ButtonNormal className="ButtonNormal submit" text="正式提交" svg={paperplane} onClick={this.handleRealSubmitClick}/>
+          </div>
+        </Modal>
       </div>
     );
 
